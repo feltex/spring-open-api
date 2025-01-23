6fd43328966a4a47eb54cdfd136f0c4f3354c9d1
Andre Felix <andrefelix.br@gmail.com>
2025-01-23 13:18:34 +0000

    Primeiro Commit

---
+++ src/test/java/br/com/feltex/spring/open/api/SpringOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feltex.spring.open.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
